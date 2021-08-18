d2a3c6d28b27257e7842d9617a0fb804b9612939
kryvoboker <kamaz_cs@live.ru>
2021-08-18 22:26:36 +0300

    edit faq

--- app/js/faqPage/faq.js
+++ app/js/faqPage/faq.js
@@ -4,16 +4,38 @@ for (let i = 0; i < titles.length; i++) {
 	const el = titles[i];
 	el.onclick = function () {
 
+		for (let k = 0; k < titles.length; k++) {
+			const element = titles[k];
+			if (element.classList.contains('active')) {
+				element.classList.remove('active');
+			}
+			if (!element.classList.contains('active')) {
+				titles[i].classList.add('active');
+			}
+		}
 		for (let j = 0; j < text.length; j++) {
 			const elem = text[j];
 			if (elem.classList.contains('active')) {
 				elem.classList.remove('active');
-				document.querySelector('.question__row+.question__row').classList.remove('active');
+				// document.querySelector('.question__row+.question__row').classList.remove('active');
 			}
-			if (!elem.classList.contains('active')) {
+			if (!text[i].classList.contains('active')) {
+				document.querySelector('.question__inner').classList.add('active');
 				document.querySelector('.question__row+.question__row').classList.add('active');
 				text[i].classList.add('active');
 			}
+
 		}
 	};
 }
+
+document.querySelector('.question__btn').onclick = function () {
+	document.querySelector('.question__inner').classList.remove('active');
+	document.querySelector('.question__row+.question__row').classList.remove('active');
+	text.forEach(el => {
+		el.classList.remove('active');
+	});
+	titles.forEach(el => {
+		el.classList.remove('active');
+	});
+};
